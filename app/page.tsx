e89db792a61f32df021f49845afb26f09e712cc9
Brando <bbaker.sh@gmail.com>
2025-01-03 15:08:37 -0800

    Removed lighroom api

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,15 @@
 // app/page.tsx
 import Globe from "./components/Globe";
-import { getLightroomPhotos } from "../utils/lightroom";
+
 
 
 export default async function Home() {
-  try {
-      const photos = await getLightroomPhotos();
-      
+ 
       return (
           <div className="min-h-screen w-full flex flex-col items-center justify-center bg-black">
               <Globe />
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-                  {photos.resources?.map((photo) => (
-                      <div key={photo.id} className="relative aspect-square">
-                          <img
-                              src={photo.links["2048"]}
-                              alt="Lightroom photo"
-                              className="object-cover w-full h-full"
-                          />
-                      </div>
-                  ))}
-              </div>
           </div>
       );
-  } catch (error) {
-      console.error("Error:", error);
-      return <div>Error loading photos. Please try again later.</div>;
-  }
+
 }
 

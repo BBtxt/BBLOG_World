aa14e650d65959ccd19c6567cb13abd6489e6217
Brando <bbaker.sh@gmail.com>
2025-01-03 16:04:38 -0800

    Added CLDN and tested need to refactor ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
-// app/page.tsx
+"use client";
 import Globe from "./components/Globe";
 
-
-
-export default async function Home() {
- 
-      return (
-          <div className="min-h-screen w-full flex flex-col items-center justify-center bg-black">
-              <Globe />
-          </div>
-      );
-
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full flex flex-col items-center justify-center bg-black">
+      <Globe />
+    </div>
+  );
 }
-
